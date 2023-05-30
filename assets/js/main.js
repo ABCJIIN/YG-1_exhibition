ec557cab0479c9c2c92b0f88575869f4ba33d9d0
ABCJIIN <97517088+ABCJIIN@users.noreply.github.com>
2023-05-30 11:45:20 +0900

    0530/11:45/jinny

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // $("#header").load("html/header.html");//헤더 INCLUDE
     $("#footer").load("html/footer.html");//푸터 INCLUDE
     $("#quickMenu").load("html/quick_menu.html");//퀵메뉴 INCLUDE
 });
