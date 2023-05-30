8cb9296d340a5438de1d9123ad02826ad086af31
ABCJIIN <97517088+ABCJIIN@users.noreply.github.com>
2023-05-30 17:55:16 +0900

    0530/17:55/jinny

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
 $(document).ready(function(){
-    // $("#header").load("html/header.html");//헤더 INCLUDE
-    $("#footer").load("html/footer.html");//푸터 INCLUDE
-    $("#quickMenu").load("html/quick_menu.html");//퀵메뉴 INCLUDE
+    $("#footer").load("html/include/footer.html");//푸터 INCLUDE
 });
---
+++ assets/js/sub.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#header").load("../../html/include/header.html");//헤더 INCLUDE
+    $("#footer").load("../../html/include/footer.html");//푸터 INCLUDE
+    $("#quickMenu").load("../../html/include/quick_menu.html");//퀵메뉴 INCLUDE
+});
